c34e3daaf0a7e356942c8390aca31b1e5b92dbdc
AlexeyParshin <dip1771@gmail.com>
2025-04-11 22:06:13 +0300

    working poc of system audio recording

--- app/src/main/java/com/idc/audio_playback_poc/MainActivity.kt
+++ app/src/main/java/com/idc/audio_playback_poc/MainActivity.kt
@@ -4,7 +4,10 @@ import android.Manifest.permission.RECORD_AUDIO
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.*
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioPlaybackCaptureConfiguration
+import android.media.AudioRecord
 import android.media.AudioRecord.STATE_INITIALIZED
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
@@ -17,13 +20,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class MainActivity : ComponentActivity() {
     @Volatile
     private var started = false
+
+    private var BUFFER_SIZE_RECORDING: Int = 0
+
     private lateinit var mediaProjectionManager: MediaProjectionManager
     private lateinit var mediaProjection: MediaProjection
     private lateinit var audioRecord: AudioRecord
@@ -72,7 +79,7 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("MissingPermission")
     fun setupAudioRecord(mediaProjection: MediaProjection): AudioRecord {
         val sampleRate = 44100
-        val channelConfig = AudioFormat.CHANNEL_IN_MONO
+        val channelConfig = AudioFormat.CHANNEL_IN_STEREO //stereo not tested
         val encoding = AudioFormat.ENCODING_PCM_16BIT
 
         val config = AudioPlaybackCaptureConfiguration.Builder(mediaProjection)
@@ -85,110 +92,155 @@ class MainActivity : ComponentActivity() {
             .setChannelMask(channelConfig)
             .build()
 
-        val bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, encoding)
+        BUFFER_SIZE_RECORDING = AudioRecord.getMinBufferSize(sampleRate, channelConfig, encoding)
 
         return AudioRecord.Builder()
             .setAudioFormat(format)
             .setAudioPlaybackCaptureConfig(config)
-            .setBufferSizeInBytes(bufferSize * 4)
+            .setBufferSizeInBytes(BUFFER_SIZE_RECORDING * 4)
             .build()
     }
 
 
     fun startAudioRecording(mediaProjection: MediaProjection) {
         audioRecord = setupAudioRecord(mediaProjection)
-        val encoder = setupAacEncoder()
-        encoder.start()
 
-        val buffer = ByteArray(2048)
-
-        val outputFile = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
-            "MyRecord.aac"
+        val pcmFile = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString(),
+            "MyRecordPCM.pcm"
         )
-        val outputStream = FileOutputStream(outputFile)
+
+        if (audioRecord.state == STATE_INITIALIZED) {
+            audioRecord.startRecording()
+        } else {
+            Log.e("AudioRecord is not initialized ", audioRecord.state.toString())
+            return
+        }
 
         Thread {
-            if (audioRecord.state == STATE_INITIALIZED) {
-                audioRecord.startRecording()
-            } else {
-                Log.e("AudioRecord is not initialized ", audioRecord.state.toString())
-                return@Thread
-            }
-
-            val bufferInfo = MediaCodec.BufferInfo()
-
-            while (started) {
-                val inputIndex = encoder.dequeueInputBuffer(10000)
-                if (inputIndex >= 0) {
-                    val inputBuffer = encoder.getInputBuffer(inputIndex)
-                    inputBuffer?.clear()
-
-                    val readBytes = audioRecord.read(buffer, 0, buffer.size)
-                    if (readBytes > 0) {
-                        inputBuffer?.put(buffer, 0, readBytes)
-                        encoder.queueInputBuffer(inputIndex, 0, readBytes, System.nanoTime() / 1000, 0)
-                    }
-                }
-
-                var outputIndex = encoder.dequeueOutputBuffer(bufferInfo, 0)
-                while (outputIndex >= 0) {
-                    val outputBuffer = encoder.getOutputBuffer(outputIndex) ?: break
-                    val outData = ByteArray(bufferInfo.size)
-                    outputBuffer.get(outData)
-                    outputBuffer.clear()
-
-                    if (bufferInfo.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG == 0) {
-                        outputStream.write(outData)
-                    }
-
-                    encoder.releaseOutputBuffer(outputIndex, false)
-                    outputIndex = encoder.dequeueOutputBuffer(bufferInfo, 0)
-                }
-            }
-
+            writeAudioDataToFile(pcmFile)
             // Cleanup (on stop):
-            encoder.stop()
-            encoder.release()
             audioRecord.stop()
             audioRecord.release()
-            outputStream.close()
         }.start()
     }
 
-    fun setupAacEncoder(): MediaCodec {
-        val sampleRate = 44100
-        val channelCount = 1
-
-        val format = MediaFormat.createAudioFormat(MediaFormat.MIMETYPE_AUDIO_AAC, sampleRate, channelCount)
-        format.setInteger(MediaFormat.KEY_BIT_RATE, 64000)
-        format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC)
-        format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 16384)
-
-        val encoder = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC)
-        encoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
-        encoder.setCallback(object : MediaCodec.Callback() {
-            override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
-                TODO("Not yet implemented")
+    private fun writeAudioDataToFile(pcmFile: File) {
+        // assign size so that bytes are read in in chunks inferior to AudioRecord internal buffer size
+        val data = ByteArray(BUFFER_SIZE_RECORDING / 2)
+        var outputStream: FileOutputStream? = null
+        try {
+            outputStream = FileOutputStream(pcmFile)
+        } catch (e: FileNotFoundException) {
+            return
+        }
+        while (started) {
+            val read = audioRecord.read(data, 0, data.size)
+            try {
+                outputStream.write(data, 0, read)
+                // clean up file writing operations
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
+        }
+        try {
+            outputStream.flush()
+            outputStream.close()
+        } catch (e: IOException) {
+            Log.e("TAG", "exception while closing output stream $e")
+            e.printStackTrace()
+        }
+        val wavFile = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString(),
+            "MyRecordWav.wav"
+        )
+        rawToWave(pcmFile, wavFile, 44100)
+    }
 
-            override fun onOutputBufferAvailable(
-                codec: MediaCodec,
-                index: Int,
-                info: MediaCodec.BufferInfo
-            ) {
-                TODO("Not yet implemented")
+    @Throws(IOException::class)
+    private fun rawToWave(rawFile: File, waveFile: File, sampleRate: Int) {
+        val rawData = ByteArray(rawFile.length().toInt())
+        var input: DataInputStream? = null
+        try {
+            input = DataInputStream(FileInputStream(rawFile))
+            input.read(rawData)
+        } finally {
+            input?.close()
+        }
+        var output: DataOutputStream? = null
+        try {
+            output = DataOutputStream(FileOutputStream(waveFile))
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF") // chunk id
+            writeInt(output, 36 + rawData.size) // chunk size
+            writeString(output, "WAVE") // format
+            writeString(output, "fmt ") // subchunk 1 id
+            writeInt(output, 16) // subchunk 1 size
+            writeShort(output, 1.toShort()) // audio format (1 = PCM)
+            writeShort(output, 1.toShort()) // number of channels
+            writeInt(output, sampleRate) // sample rate
+            writeInt(output, sampleRate * 2) // byte rate     --------- Here changen maybe
+            writeShort(output, 2.toShort()) // block align
+            writeShort(output, 16.toShort()) // bits per sample
+            writeString(output, "data") // subchunk 2 id
+            writeInt(output, rawData.size) // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            val shorts = ShortArray(rawData.size / 2)
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts)
+            val bytes: ByteBuffer = ByteBuffer.allocate(shorts.size * 2)
+            for (s in shorts) {
+                bytes.putShort(s)
             }
+            output.write(fullyReadFileToBytes(rawFile))
+        } finally {
+            output?.close()
+        }
+    }
 
-            override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
-                TODO("Not yet implemented")
+    @Throws(IOException::class)
+    fun fullyReadFileToBytes(f: File): ByteArray? {
+        val size = f.length().toInt()
+        val bytes = ByteArray(size)
+        val tmpBuff = ByteArray(size)
+        val fis = FileInputStream(f)
+        try {
+            var read = fis.read(bytes, 0, size)
+            if (read < size) {
+                var remain = size - read
+                while (remain > 0) {
+                    read = fis.read(tmpBuff, 0, remain)
+                    System.arraycopy(tmpBuff, 0, bytes, size - remain, read)
+                    remain -= read
+                }
             }
+        } catch (e: IOException) {
+            throw e
+        } finally {
+            fis.close()
+        }
+        return bytes
+    }
 
-            override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
-                TODO("Not yet implemented")
-            }
-        })
+    @Throws(IOException::class)
+    private fun writeInt(output: DataOutputStream, value: Int) {
+        output.write(value shr 0)
+        output.write(value shr 8)
+        output.write(value shr 16)
+        output.write(value shr 24)
+    }
 
-        return encoder
+    @Throws(IOException::class)
+    private fun writeShort(output: DataOutputStream, value: Short) {
+        var v = value.toInt()
+        output.write(v shr 0)
+        output.write(v shr 8)
+    }
+
+    @Throws(IOException::class)
+    private fun writeString(output: DataOutputStream, value: String) {
+        for (i in 0 until value.length) {
+            output.write(value[i].code)     //  ------ Here changen maybe
+        }
     }
 }
